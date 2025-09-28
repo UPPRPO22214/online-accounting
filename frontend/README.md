500635e08cd1df51cf1fa6daa155d231ddc619df
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2025-09-28 20:13:35 +0700

    Init basic env, edit README, install TS, react, vite, tailwind

---
+++ frontend/README.md
@@ -0,0 +1,16 @@
+# Фронтэнда проекта `online-accounting`
+## Стэк
+- `bun 1.2.13`
+- `node 24.9.0`
+
+## Разработка
+```bash
+bun i --frozen-lockfile
+
+bun run dev
+```
+
+## Сборка
+```bash
+bun run build
+```
---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': resolve('./src'),
+      '@public': resolve('./public'),
+      $fonts: resolve('./public/fonts'),
+    },
+  },
+})
