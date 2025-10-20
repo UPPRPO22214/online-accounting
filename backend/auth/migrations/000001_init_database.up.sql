60b0496ff31511ef1ca66724c4f8b1396d8b08b6
Anton Tsoy <tcoy210@gmail.com>
2025-10-21 00:23:12 +0700

    Fix: extand logic of db queries on refresh_tokens table

--- backend/auth/migrations/000001_init_database.up.sql
+++ backend/auth/migrations/000001_init_database.up.sql
@@ -1,15 +1,16 @@
 CREATE TABLE IF NOT EXISTS users (
     id               INT PRIMARY KEY AUTO_INCREMENT,
     email            VARCHAR(64) UNIQUE NOT NULL,
-    password_hash    VARCHAR(32) NOT NULL
+    password_hash    VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS refresh_tokens (
-    id              INT PRIMARY KEY AUTO_INCREMENT,
     user_id         INT NOT NULL,
-    token_hash      VARCHAR(32) NOT NULL,
+    token_hash      VARCHAR(64) NOT NULL,
     is_refreshed    BOOLEAN NOT NULL DEFAULT FALSE,  -- флаг: был ли использован этот токен для обновления access токена
     created_at      TIMESTAMP NOT NULL DEFAULT NOW(),
     expires_at      TIMESTAMP NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    revoked_at      TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, token_hash)
 );
--- backend/auth/query.sql
+++ backend/auth/query.sql
@@ -5,7 +5,7 @@ WHERE email = ?
 LIMIT 1;
 
 -- name: CheckUserByID :one
-SELECT COUNT(*) > 0 AS user_exists
+SELECT COUNT(*) = 1 AS user_exists
 FROM users
 WHERE id = ?;
 
@@ -17,3 +17,25 @@ VALUES (?, ?, ?);
 UPDATE users
 SET password_hash = ?
 WHERE id = ?;
+
+-- name: GenerateRefreshToken :exec
+INSERT INTO refresh_tokens (user_id, token_hash, expires_at)
+VALUES (?, ?, ?);
+
+-- name: UseValidRefreshToken :execresult
+UPDATE refresh_tokens
+SET is_refreshed = TRUE
+WHERE user_id = ?
+      AND token_hash = ?
+      AND is_refreshed = FALSE
+      AND expires_at > NOW();
+
+-- name: RevokeRefreshToken :exec
+UPDATE refresh_tokens
+SET revoked_at = NOW()
+WHERE user_id = ? AND token_hash = ?;
+
+-- name: GetRefreshToken :one
+SELECT * FROM refresh_tokens
+WHERE user_id = ? AND token_hash = ?
+LIMIT 1;
