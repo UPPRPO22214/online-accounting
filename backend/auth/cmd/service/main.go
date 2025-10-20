9f1bd79bd7a387b39f8eac21e3e905136f8a4314
Anton Tsoy <tcoy210@gmail.com>
2025-10-20 13:34:02 +0700

    Add: Init auth service and migrations

---
+++ backend/auth/cmd/service/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// TODO: Repo connect with MySQL
+
+	// TODO: Service for auth users
+
+	// TODO: Transport, proto files for gRPC
+}
---
+++ backend/auth/migrations/000001_init_database.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+    id               INT PRIMARY KEY AUTO_INCREMENT,
+    email            VARCHAR(64) UNIQUE NOT NULL,
+    password_hash    VARCHAR(32) NOT NULL,
+    nickname         VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+    id              INT PRIMARY KEY AUTO_INCREMENT,
+    user_id         INT NOT NULL,
+    token_hash      VARCHAR(32) NOT NULL,
+    is_refreshed    BOOLEAN NOT NULL DEFAULT FALSE,  -- флаг: был ли использован этот токен для обновления access токена
+    created_at      TIMESTAMP NOT NULL DEFAULT NOW(),
+    expires_at      TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
